1964ac7c75a40396ccddd25997ab77935dd24b2b
No Nope <notatgamingg@protonmail.com>
2021-10-10 21:14:27 +0200

    Fixed setup (again)

--- setup.sh
+++ setup.sh
@@ -36,15 +36,14 @@ git clone $AUR_SOURCE/pokemon-colorscripts-git
 cd paru-bin && makepkg -si --noconfirm && cd ../ && rm -rf paru-bin
 cd vimix-cursors && makepkg -si --noconfirm && cd ../ && rm -rf vimix-cursors
 #cd libreddit-git && makepkg -si --noconfirm && cd ../ && rm -rf libreddit-git
-cd librewolf-bin && makepkg -si --noconfirm && cd ../ && rm -rf librewolf-bin
+cd librewolf-bin && makepkg -si --skipinteg --noconfirm && cd ../ && rm -rf librewolf-bin
 cd pokemon-colorscripts-git && makepkg -si --noconfirm && cd ../ && rm -rf pokemon-colorscripts-git
 # End from install stuff from the AUR
 ##
 mkdir -p $HOME/.local/bin && cd $HOME/.local/bin && ln -f /usr/local/opt/pokemon-colorscripts/pokemon-colorscripts.sh pokemon-colorscripts
 chmod +x pokemon-colorscripts
 ##
-cp -r $HOME/.config $HOME/.config.bak
-rm -rf $HOME/.config
+[ -f $HOME/.config ] && cp -r $HOME/.config $HOME/.config.bak && rm -rf $HOME/.config
 cp -R ./.config $HOME/.config
 
 ## Change some core stuff
