01c832b25474b0fa42528eedd4abb0e555740c3d
No Nope <notatgamingg@protonmail.com>
2021-10-20 17:40:06 +0200

    Improved setup script layout

--- setup.sh
+++ setup.sh
@@ -11,29 +11,31 @@ GTK_COPY_NAME="Lightningbug-Dark" # What to copy from the themes dir
 AUR_SOURCE="https://aur.archlinux.org"
 
 [ "$(id -u)" = "0" ] && echo "Do not run this as root as it can cause damage!" && exit 1
-echo -e "\nNote: you need to have a working Internet connection.\n"
+echo -e "Note: you need to have a working Internet connection."
 echo "Are you sure you want to install my dotfiles?. THIS WILL REPLACE YOU ~/.config, ~/.zprofile AND /etc/pacman.conf!"
 read ques
 [ "$ques" != "y" ] && echo "Cancelling setup. user said no"  && exit 1
  #Add Arch Repos
  #Artix add arch repos ( artix specific)
 artix_spec() {
-	echo "Adding Arch repos."
-	sudo pacman -S --needed --noconfirm artix-archlinux-support
-	sudo pacman-key --populate archlinux
-	sudo rm /etc/pacman.conf && sudo cp ./pacman-artix.conf /etc/pacman.conf
-	echo "Added Arch repos."
+	[ -f "/etc/pacman.d/mirrorlist-arch" ] || echo "Adding Arch repos." && sudo pacman -S --needed --noconfirm artix-archlinux-support && sudo pacman-key --populate archlinux && echo "Added Arch repos"
+	 sudo cp ./pacman-artix.conf /etc/pacman.conf
 }
 # For Arch
 arch_spec() {
-	sudo rm /etc/pacman.conf && sudo cp ./pacman-arch /etc/pacman.conf
+	sudo cp ./pacman-arch /etc/pacman.conf
 	echo "Added multilib to repo pool"
 }
 #Add chaotic-aur
-echo "Adding the chaotic AUR"
-sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key 3056513887B78AEB
-sudo pacman -U --needed --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+add_chaotic() { 
+	 echo "Adding the chaotic AUR"
+	 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+	 sudo pacman-key --lsign-key 3056513887B78AEB
+	 sudo pacman -U --needed --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+}
+
+[ -f "/etc/pacman.d/chaotic-mirrorlist" ] || add_chaotic || echo "Error adding chaotic-aur" && exit 1
 
 # Distro Stuff
 [ "$distro" = "Artix" ] && artix_spec
@@ -44,9 +46,13 @@ sudo pacman -Syu --noconfirm --ask 4
 echo "Installing Programs"
 sudo pacman -S --needed --noconfirm --ask 4 - < Pkgs.txt
 #Installing Pokemon colorscripts from the AUR
-[ -f ./pokemon-colorscripts-git ] && rm -rfv pokemon-colorscripts-git
-git clone $AUR_SOURCE/pokemon-colorscripts-git.git && cd pokemon-colorscripts-git && makepkg -si --noconfirm --needed && cd .. && rm -rf pokemon-colorscripts-git
-#End from install stuff from the AUR
+
+pokemon() {
+	[ -f ./pokemon-colorscripts-git ] && rm -rfv pokemon-colorscripts-git
+	git clone $AUR_SOURCE/pokemon-colorscripts-git.git && cd pokemon-colorscripts-git && makepkg -si --noconfirm --needed && cd .. && rm -rf pokemon-colorscripts-git
+}
+
+
 echo "~/.config IS GOING TO BE DELETED NOW. A BACKUP IS IN ~/.config.bak"
 echo "Moving Wallpaper to /opt"
 sudo mv "./City.jpg" /opt/Paper.jpg
@@ -56,11 +62,9 @@ cp -R "./.config" "$HOME/.config"
 
 ## Installing Theme
 git clone $GTK_THEME_SOURCE
-sudo mv $GTK_THEME_NAME/$GTK_COPY_THEME/usr/share/themes
+sudo mv $GTK_THEME_NAME/$GTK_COPY_THEME /usr/share/themes
 echo "Installed $GTK_THEME_NAME"
 #Change some core stuff
-sudo touch /etc/doas.conf
-sudo echo "permit :wheel" > /etc/doas.conf
 echo "Copying .zprofile to ~"
 sudo cp ./zprofile $HOME/.zprofile
 echo "Changing shell"
